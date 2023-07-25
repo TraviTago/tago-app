953abb44db19d112be50e78c4caa7a0d8806cb12
sunxsoo <njs783075@gmail.com>
2023-07-25 15:23:54 +0900

    chore: 색상 설정 (#2)

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const PRIMARY_COLOR = Color(0xFFE63E43);
+// 텍스트필드 배경 색상
+const BUTTON_BG_COLOR = Color(0xFF747474);
+// 텍스트필드 테두리 색상
+const LABEL_BG_COLOR = Color(0xFFF5F5F5);
