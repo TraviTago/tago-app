81e0e5cf6b40446d69933e4a56dc1b9841deb463
sunxsoo <njs783075@gmail.com>
2023-07-25 16:12:16 +0900

    feat: go_router 설정 및 initial 페이지 (spalshScreen) 지정 (#3)

---
+++ lib/common/view/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:tago_app/common/const/colors.dart';
+import 'package:tago_app/common/layout/default_layout.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  static String get routeName => 'splash';
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: const Center(
+          child: SlideTransitionExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class SlideTransitionExample extends StatefulWidget {
+  const SlideTransitionExample({super.key});
+
+  @override
+  State<SlideTransitionExample> createState() => _SlideTransitionExampleState();
+}
+
+class _SlideTransitionExampleState extends State<SlideTransitionExample>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 4),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(1.5, 0.0),
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticIn,
+  ));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset(
+          'asset/img/logo.png',
+          width: MediaQuery.of(context).size.width / 2,
+        ),
+      ),
+    );
+  }
+}
