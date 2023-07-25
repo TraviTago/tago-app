e0f7a476eebcc7eaef19cc1797964372e722fc01
sunxsoo <njs783075@gmail.com>
2023-07-25 18:45:42 +0900

    feat: 홈 화면 및 하단 네비게이션 추가 (#3)

---
+++ lib/common/view/root_tab.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:tago_app/common/const/colors.dart';
+import 'package:tago_app/common/layout/default_layout.dart';
+import 'package:tago_app/travel/view/home_screen.dart';
+
+class RootTab extends StatefulWidget {
+  const RootTab({Key? key}) : super(key: key);
+  static String get routeName => 'home';
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 3, vsync: this);
+
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: Colors.black,
+        iconSize: 28,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 10,
+          fontWeight: FontWeight.w600,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontSize: 10,
+          fontWeight: FontWeight.w500,
+        ),
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          controller.animateTo(index);
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.map_outlined,
+            ),
+            label: '내여행',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.sentiment_satisfied_alt_outlined,
+            ),
+            label: '마이페이지',
+          ),
+        ],
+      ),
+      child: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          const HomeScreen(),
+          Center(child: Container(child: const Text('내여행'))),
+          Center(child: Container(child: const Text('마이페이지'))),
+        ],
+      ),
+    );
+  }
+}
