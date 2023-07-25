9318f8f405416fcb0bb8da81b7b72bb077fd6eff
sunxsoo <njs783075@gmail.com>
2023-07-25 18:46:11 +0900

    feat: 홈 화면의 상단 네비게이션 추가 (#3)

---
+++ lib/travel/view/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  static String get routeName => 'home';
+
+  @override
+  State<HomeScreen> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Material(
+              color: Colors.white,
+              elevation: 0,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  isScrollable: true,
+                  padding: const EdgeInsets.only(left: 30.0),
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelStyle: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18.0,
+                  ),
+                  labelStyle: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18.0,
+                  ),
+                  indicator: const UnderlineTabIndicator(
+                    insets: EdgeInsets.symmetric(horizontal: 6.0),
+                    borderRadius: BorderRadius.all(Radius.circular(1.0)),
+                    borderSide: BorderSide(
+                      width: 3.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  tabs: const <Widget>[
+                    Tab(text: '참여하기'),
+                    Tab(text: '둘러보기'),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Center(child: Container(child: const Text('참여하기'))),
+                  Center(child: Container(child: const Text('둘러보기'))),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
