94ac84dba01c2e0d2529fb0290c80edd9b6a78b0
sunxsoo <njs783075@gmail.com>
2023-07-25 18:46:48 +0900

    feat: 화면 등록 및 라우팅 설정 (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tago_app/common/view/root_tab.dart';
 import 'package:tago_app/common/view/splash_screen.dart';
+import 'package:tago_app/form/view/first_form_screen.dart';
+import 'package:tago_app/form/view/last_form_screen.dart';
+import 'package:tago_app/form/view/second_from_screen.dart';
+import 'package:tago_app/form/view/third_form_screen.dart';
 
 void main() {
   runApp(
@@ -21,6 +26,31 @@ class _App extends StatelessWidget {
         path: '/splash',
         name: SplashScreen.routeName,
         builder: (_, __) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: '/form1',
+        name: FirstFormScreen.routeName,
+        builder: (_, __) => const FirstFormScreen(),
+      ),
+      GoRoute(
+        path: '/form2',
+        name: SecondFormScreen.routeName,
+        builder: (_, __) => const SecondFormScreen(),
+      ),
+      GoRoute(
+        path: '/form3',
+        name: ThirdFormScreen.routeName,
+        builder: (_, __) => const ThirdFormScreen(),
+      ),
+      GoRoute(
+        path: '/form4',
+        name: LastFormScreen.routeName,
+        builder: (_, __) => const LastFormScreen(),
+      ),
+      GoRoute(
+        path: '/',
+        name: RootTab.routeName,
+        builder: (_, __) => const RootTab(),
       ),
     ],
   );
