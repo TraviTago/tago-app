845f3cfe618d6b90e5a01ba486c880b7c41771ce
sunxsoo <njs783075@gmail.com>
2023-08-14 04:57:16 +0900

    fix: default_layout을 코스 상세화면 appBar에 맞게 수정한다 (#14)

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -4,12 +4,14 @@ class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
   final String? title;
+  final Widget? titleComponet;
   final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     required this.child,
     this.backgroundColor,
     this.title,
+    this.titleComponet,
     this.bottomNavigationBar,
     Key? key,
   }) : super(key: key);
@@ -18,14 +20,34 @@ class DefaultLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
-      appBar: renderAppBar(),
+      appBar: renderAppBar(context),
       body: child,
       bottomNavigationBar: bottomNavigationBar,
     );
   }
 
-  AppBar? renderAppBar() {
-    if (title == null) {
+  AppBar? renderAppBar(BuildContext context) {
+    if (titleComponet != null) {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: IconButton(
+            icon: const Icon(
+              Icons.chevron_left_rounded,
+              color: Colors.black,
+              size: 34,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        title: titleComponet,
+        foregroundColor: Colors.black,
+      );
+    } else if (title == null) {
       return null;
     } else {
       return AppBar(
