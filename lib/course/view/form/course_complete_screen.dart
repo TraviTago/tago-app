f86b29c2f980bd52767e4d6782df26fac3fab5e1
sunxsoo <njs783075@gmail.com>
2023-08-15 20:08:55 +0900

    feat: 코스 생성 완료 화면 구현 (#9)

--- lib/course/view/form/course_complete_screen.dart
+++ lib/course/view/form/course_complete_screen.dart
@@ -1,19 +1,140 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tago_app/common/const/colors.dart';
+import 'package:tago_app/common/const/data.dart';
 import 'package:tago_app/common/layout/default_layout.dart';
+import 'package:tago_app/course/component/course_recommend_card.dart';
 
-class CourseCompleteScreen extends StatelessWidget {
+class CourseCompleteScreen extends StatefulWidget {
   static String get routeName => 'courseComplete';
 
   const CourseCompleteScreen({super.key});
 
   @override
+  State<CourseCompleteScreen> createState() => _CourseCompleteScreenState();
+}
+
+bool isCheckedCourse = false;
+
+class _CourseCompleteScreenState extends State<CourseCompleteScreen> {
+  @override
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
+    return DefaultLayout(
       backBtnComponent: true,
-      child: Center(
-        child: Text(
-          "성",
+      child: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '짜잔! 완성되었어요',
+                    style: TextStyle(
+                      fontSize: 23.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    '클릭해서 취향에 맞게 수정해보세요 ',
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      color: Color(0xFF595959),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  CourseRecommendCard.fromModel(
+                    model: courseData,
+                    isCompleteCompnent: true,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Transform.scale(
+                        // Checkbox 크기 조정
+                        scale: 1.2,
+                        child: Checkbox(
+                          activeColor: PRIMARY_COLOR,
+                          side: const BorderSide(
+                            width: 1,
+                            color: Color(0xFFDADADA),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          value: isCheckedCourse,
+                          onChanged: (bool? newValue) {
+                            setState(() {
+                              isCheckedCourse = newValue ?? false;
+                            });
+                          },
+                        ),
+                      ),
+                      const Text(
+                        '코스 상세정보를 모두 확인했어요',
+                        style: TextStyle(
+                          color: Color(0xFF595959),
+                          fontSize: 13.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 30,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        Size(MediaQuery.of(context).size.width, 45)),
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor: !isCheckedCourse
+                        ? MaterialStateProperty.all(const Color(0xFFF5F5F5))
+                        : MaterialStateProperty.all(PRIMARY_COLOR),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
+                  onPressed: !isCheckedCourse
+                      ? null
+                      : () {
+                          context.go('/');
+                        },
+                  child: Text(
+                    '내 여행으로 추가하기',
+                    style: !isCheckedCourse
+                        ? const TextStyle(
+                            color: Color(0xFF595959),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16.0)
+                        : const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
