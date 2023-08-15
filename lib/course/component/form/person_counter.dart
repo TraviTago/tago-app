99c25552f233878df74e0286b3e641af0753f232
sunxsoo <njs783075@gmail.com>
2023-08-15 18:52:26 +0900

    fix: 버튼 별 인원 최소, 최대 및 색상 변경 로직 추가 (#16)

--- lib/course/component/form/person_counter.dart
+++ lib/course/component/form/person_counter.dart
@@ -4,12 +4,18 @@ class PersonCounter extends StatefulWidget {
   final int count;
   final VoidCallback onIncrement;
   final VoidCallback onDecrement;
+  final Color textColor;
+  final int min; // 최소값 속성 추가
+  final int max; // 최대값 속성 추가
 
   const PersonCounter({
     Key? key,
     required this.count,
     required this.onIncrement,
     required this.onDecrement,
+    required this.min,
+    required this.max,
+    this.textColor = Colors.black,
   }) : super(key: key);
 
   @override
@@ -39,29 +45,32 @@ class PersonCounterState extends State<PersonCounter> {
           children: [
             IconButton(
               splashRadius: 18,
-              onPressed: widget.count > 1 ? widget.onDecrement : null,
+              onPressed: widget.count > widget.min ? widget.onDecrement : null,
               icon: Icon(
                 Icons.remove,
                 size: 18.0,
-                color: widget.count > 1
+                color: widget.count > widget.min
                     ? const Color(0xFF595959)
                     : const Color(0xFFDADADA),
               ),
             ),
             Text(
               '${widget.count}',
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.w700,
+                color: widget.textColor,
               ),
             ),
             IconButton(
               splashRadius: 18,
-              onPressed: widget.onIncrement,
-              icon: const Icon(
+              onPressed: widget.count < widget.max ? widget.onIncrement : null,
+              icon: Icon(
                 Icons.add,
                 size: 18.0,
-                color: Color(0xFF595959),
+                color: widget.count < widget.max
+                    ? const Color(0xFF595959)
+                    : const Color(0xFFDADADA),
               ),
             ),
           ],
