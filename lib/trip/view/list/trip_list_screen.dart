3bb1afcd104e9881c4d3ac3135b6367636925392
sunxsoo <njs783075@gmail.com>
2023-08-25 20:21:00 +0900

    feat: list pagination refetching 적용 (#24)

--- lib/trip/view/list/trip_list_screen.dart
+++ lib/trip/view/list/trip_list_screen.dart
@@ -68,110 +68,122 @@ class _TripListScreenState extends ConsumerState<TripListScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-        child: ListView.builder(
-          controller: controller,
-          padding: const EdgeInsets.symmetric(vertical: 20.0),
-          itemCount: cp.contents.length + 1,
-          itemBuilder: (_, index) {
-            if (index == cp.contents.length) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8.0,
-                  horizontal: 16.0,
-                ),
-                child: Center(
-                  child: contents is CursorPaginationFetchingMore
-                      ? const CircularProgressIndicator()
-                      : Container(
-                          width: 40,
-                          height: 40,
-                          decoration: const BoxDecoration(
-                            color: LABEL_BG_COLOR,
-                            shape: BoxShape.circle,
-                          ),
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.arrow_upward,
-                              color: LABEL_TEXT_SUB_COLOR,
-                              size: 24.0,
-                            ),
-                            onPressed: () {
-                              controller.animateTo(
-                                0.0, // 맨 위로 이동
-                                duration: const Duration(milliseconds: 300),
-                                curve: Curves.bounceIn,
-                              );
-                            },
-                          ),
-                        ),
-                ),
-              );
-            }
-
-            DateTime? previousDate;
-            if (index - 1 >= 0) {
-              previousDate = cp.contents[index - 1].dateTime;
-            }
-
-            final bool shouldShowDate = previousDate == null ||
-                !DataUtils.isSameDate(previousDate,
-                    cp.contents[index].dateTime); // 도우미 함수를 사용하여 같은 날짜인지 확인
-
-            if (index == 0) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    '당신에게 딱 맞는 여행!',
-                    style: TextStyle(
-                      fontSize: 19.0,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  TripRecommendCard.fromModel(model: tripData),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    DataUtils.formatDate(cp.contents[index].dateTime),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  TripCard.fromModel(
-                    model: cp.contents[index],
-                  ),
-                ],
-              );
-            }
-            if (shouldShowDate) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    DataUtils.formatDate(cp.contents[index].dateTime),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  TripCard.fromModel(model: cp.contents[index]),
-                ],
-              );
-            } else {
-              return TripCard.fromModel(model: cp.contents[index]);
-            }
+        child: RefreshIndicator(
+          color: Colors.grey,
+          onRefresh: () async {
+            await ref.read(tripProvider.notifier).paginate(forceRefetch: true);
           },
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return ScaleTransition(scale: animation, child: child);
+            },
+            child: ListView.builder(
+              controller: controller,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              itemCount: cp.contents.length + 1,
+              itemBuilder: (_, index) {
+                if (index == cp.contents.length) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 16.0,
+                    ),
+                    child: Center(
+                      child: contents is CursorPaginationFetchingMore
+                          ? const CircularProgressIndicator()
+                          : Container(
+                              width: 40,
+                              height: 40,
+                              decoration: const BoxDecoration(
+                                color: LABEL_BG_COLOR,
+                                shape: BoxShape.circle,
+                              ),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_upward,
+                                  color: LABEL_TEXT_SUB_COLOR,
+                                  size: 24.0,
+                                ),
+                                onPressed: () {
+                                  controller.animateTo(
+                                    0.0, // 맨 위로 이동
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.bounceIn,
+                                  );
+                                },
+                              ),
+                            ),
+                    ),
+                  );
+                }
+
+                DateTime? previousDate;
+                if (index - 1 >= 0) {
+                  previousDate = cp.contents[index - 1].dateTime;
+                }
+
+                final bool shouldShowDate = previousDate == null ||
+                    !DataUtils.isSameDate(previousDate,
+                        cp.contents[index].dateTime); // 도우미 함수를 사용하여 같은 날짜인지 확인
+
+                if (index == 0) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '당신에게 딱 맞는 여행!',
+                        style: TextStyle(
+                          fontSize: 19.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      TripRecommendCard.fromModel(model: tripData),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        DataUtils.formatDate(cp.contents[index].dateTime),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      TripCard.fromModel(
+                        model: cp.contents[index],
+                      ),
+                    ],
+                  );
+                }
+                if (shouldShowDate) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        DataUtils.formatDate(cp.contents[index].dateTime),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      TripCard.fromModel(model: cp.contents[index]),
+                    ],
+                  );
+                } else {
+                  return TripCard.fromModel(model: cp.contents[index]);
+                }
+              },
+            ),
+          ),
         ),
       ),
     );
