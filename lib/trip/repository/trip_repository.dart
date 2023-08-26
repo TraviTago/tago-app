ad1ac8a67b2d7bbc643b9d325fa912819dc47aa0
sunxsoo <njs783075@gmail.com>
2023-08-26 15:34:13 +0900

    feat: 나의 여행 조회 API 레포지토리 작성 (#27)

--- lib/trip/repository/trip_repository.dart
+++ lib/trip/repository/trip_repository.dart
@@ -5,6 +5,7 @@ import 'package:tago_app/common/const/data.dart';
 import 'package:tago_app/common/dio/dio.dart';
 import 'package:tago_app/common/model/cursor_pagination_model.dart';
 import 'package:tago_app/common/model/pagination_params.dart';
+import 'package:tago_app/trip/model/my_trip_response_model.dart';
 import 'package:tago_app/trip/model/trip_detail_model.dart';
 import 'package:tago_app/trip/model/trip_model.dart';
 
@@ -35,4 +36,10 @@ abstract class TripRepository {
   Future<TripDetailModel> getDetailTrip({
     @Path() required int tripId,
   });
+
+  @GET('/members/me')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<MyTripResponseModel> getMyTrips();
 }
