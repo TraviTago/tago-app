3f6d92fe0462dd85d1f05c1f994476a8ef16dad7
sunxsoo <njs783075@gmail.com>
2023-09-03 18:32:20 +0900

    feat: 탭 변환 간 배경 에니메이션 적용 (#33)

--- lib/common/view/home_screen.dart
+++ lib/common/view/home_screen.dart
@@ -11,18 +11,36 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeState();
 }
 
-class _HomeState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+class _HomeState extends State<HomeScreen> with TickerProviderStateMixin {
   late TabController _tabController;
+  late AnimationController _animationController;
+  late Animation<Color?> _colorAnimation;
+
   String backgroundImageUrl =
       'http://tong.visitkorea.or.kr/cms/resource/67/2612467_image2_1.jpg';
   @override
   void initState() {
     super.initState();
+
     _tabController = TabController(length: 2, vsync: this);
 
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animationController.addListener(() {
+      setState(() {}); // 여기서 setState를 호출해줍니다.
+    });
+    _colorAnimation = ColorTween(
+      begin: Colors.white,
+      end: Colors.white.withOpacity(0.0),
+    ).animate(_animationController);
+
     _tabController.addListener(() {
-      if (_tabController.indexIsChanging) {
-        setState(() {});
+      if (_tabController.index == 0) {
+        _animationController.reverse();
+      } else {
+        _animationController.forward();
       }
     });
   }
@@ -38,51 +56,47 @@ class _HomeState extends State<HomeScreen> with SingleTickerProviderStateMixin {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          if (_tabController.index == 1)
-            Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: Container(
-                  height: 500,
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(backgroundImageUrl),
-                      fit: BoxFit.cover,
+          Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Container(
+                height: 500,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(backgroundImageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.white
+                      ], // 여기서 그라데이션의 색상을 지정합니다.
                     ),
                   ),
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.white
-                        ], // 여기서 그라데이션의 색상을 지정합니다.
-                      ),
-                    ),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                      child: Container(color: Colors.white.withOpacity(0.1)),
-                    ),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(color: Colors.white.withOpacity(0.1)),
                   ),
-                )),
-          if (_tabController.index == 0)
-            Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-              ),
+                ),
+              )),
+          Container(
+            decoration: BoxDecoration(
+              color: _colorAnimation.value,
             ),
+          ),
           SafeArea(
             child: Column(
               children: <Widget>[
                 Material(
-                  color: _tabController.index == 0
-                      ? Colors.white
-                      : Colors.transparent,
+                  color: Colors.transparent,
                   elevation: 0,
                   child: Container(
                     alignment: Alignment.centerLeft,
