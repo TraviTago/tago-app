9a75ba34631fa57739f140fbd8386ea01fc52549
sunxsoo <njs783075@gmail.com>
2023-09-07 22:22:16 +0900

    fix: isJoined 값에 따라 하단 버튼, floating 버튼 분기 (#34)

--- lib/trip/view/detail/trip_detail_overview_screen.dart
+++ lib/trip/view/detail/trip_detail_overview_screen.dart
@@ -26,53 +26,56 @@ class TripDetailOverViewScreen extends ConsumerWidget {
     Future<TripStatusModel> statusModel =
         ref.watch(tripRepositoryProvider).getStatusTrip(tripId: tripId);
     return DefaultLayout(
-      //TOFIX: FloatingButton 케이스
       // 내여행일 경우: floating 버튼 없음
       // 내여행이 아닌 경우:
       // 다가오는 여행일 경우 -> 참여하기 버튼
       // 진행중인 여행 or 지난 여행일 경우  -> 참여 불가 버튼
-      floatingActionButton: tripStatus == TripStatus.upcoming
-          ? MaterialButton(
-              onPressed: () {},
-              color: PRIMARY_COLOR,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              elevation: 0,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-                child: Text(
-                  '참여하기',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
+      floatingActionButton: detailModel.isJoined == true
+          ? null
+          : tripStatus == TripStatus.upcoming
+              ? MaterialButton(
+                  onPressed: () {},
+                  color: PRIMARY_COLOR,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 0,
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                    child: Text(
+                      '참여하기',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              : ElevatedButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor: MaterialStateProperty.all(BUTTON_BG_COLOR),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )),
+                  ),
+                  onPressed: null,
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    child: Text(
+                      '마감된 여행이에요',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            )
-          : ElevatedButton(
-              style: ButtonStyle(
-                elevation: MaterialStateProperty.all(0),
-                backgroundColor: MaterialStateProperty.all(BUTTON_BG_COLOR),
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                )),
-              ),
-              onPressed: null,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                child: Text(
-                  '마감된 여행이에요',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ),
 
       child: SafeArea(
         bottom: true,
@@ -114,40 +117,46 @@ class TripDetailOverViewScreen extends ConsumerWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          MaterialButton(
-                            onPressed: () {
-                              context.push("/tripDetail/$tripId/members");
-                            },
-                            color: PRIMARY_COLOR,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                          if (detailModel.isJoined == false)
+                            const SizedBox(
+                              height: 50.0,
                             ),
-                            elevation: 0,
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 5.0, vertical: 8.0),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    '내 여행메이트 정보 보러가기 ',
-                                    style: TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.w900,
-                                      color: Colors.white,
+                          if (detailModel.isJoined == true)
+                            MaterialButton(
+                              onPressed: () {
+                                context.push("/tripDetail/$tripId/members");
+                              },
+                              color: PRIMARY_COLOR,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              elevation: 0,
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 5.0, vertical: 8.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      '내 여행메이트 정보 보러가기 ',
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w900,
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                  Icon(
-                                    Icons.chevron_right_sharp,
-                                    color: Colors.white,
-                                    size: 25.0,
-                                  ),
-                                ],
+                                    Icon(
+                                      Icons.chevron_right_sharp,
+                                      color: Colors.white,
+                                      size: 25.0,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                          if (tripStatus == TripStatus.ongoing)
+                          if (tripStatus == TripStatus.ongoing &&
+                              detailModel.isJoined == true)
                             MaterialButton(
                               onPressed: () {
                                 context.go('/customerReport');
