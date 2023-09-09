5a84f20a890f84733090c1627291689d1bdfa3ee
sunxsoo <njs783075@gmail.com>
2023-09-09 18:09:34 +0900

    feat: 여행지 전체 화면 및 검색 기능 구현 (#35)

--- lib/place/view/place_search_screen.dart
+++ lib/place/view/place_search_screen.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tago_app/common/const/colors.dart';
 import 'package:tago_app/common/layout/default_layout.dart';
+import 'package:tago_app/common/model/cursor_pagination_model.dart';
+import 'package:tago_app/place/component/place_list_card.dart';
+import 'package:tago_app/place/provider/place_provider.dart';
+import 'package:tago_app/place/model/place_summary_model.dart';
 
-class PlaceSearchScreen extends StatefulWidget {
+class PlaceSearchScreen extends ConsumerStatefulWidget {
   static String get routeName => 'places';
 
   const PlaceSearchScreen({super.key});
 
   @override
-  State<PlaceSearchScreen> createState() => _PlaceSearchScreenState();
+  ConsumerState<PlaceSearchScreen> createState() => _PlaceSearchScreenState();
 }
 
-class _PlaceSearchScreenState extends State<PlaceSearchScreen> {
+class _PlaceSearchScreenState extends ConsumerState<PlaceSearchScreen> {
   final TextEditingController controller = TextEditingController();
-  String searchedIndex = "";
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(placeProvider.notifier).resetAndFetchPlaces();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      titleComponet: Padding(
+    return DefaultLayout(
+      titleComponet: const Padding(
         padding: EdgeInsets.only(right: 15.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,8 +44,94 @@ class _PlaceSearchScreenState extends State<PlaceSearchScreen> {
           ],
         ),
       ),
-      child: Column(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30.0,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5.0,
+            ),
+            TextField(
+              controller: controller,
+              onChanged: (value) {
+                ref.read(placeProvider.notifier).filterPlaces(value);
+              },
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+                filled: true,
+                fillColor: LABEL_BG_COLOR,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: LABEL_TEXT_SUB_COLOR,
+                ),
+                hintStyle: TextStyle(
+                  color: LABEL_TEXT_SUB_COLOR,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w500,
+                ),
+                hintText: '검색하기',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    style: BorderStyle.none,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Consumer(
+                builder: (context, watch, child) {
+                  final state = ref.watch(placeProvider);
+
+                  if (state is CursorPaginationLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is CursorPaginationError) {
+                    return Text('Error: ${state.message}');
+                  } else if (state is CursorPagination) {
+                    final places = state.contents as List<PlaceSummaryModel>;
+
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                        ),
+                        itemCount: places.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 10.0),
+                            child:
+                                PlaceListCard.fromModel(model: places[index]),
+                          );
+                        },
+                      ),
+                    );
+                  } else {
+                    return const Text('No data found.');
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
