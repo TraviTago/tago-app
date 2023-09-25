4b50a636a6c1fff7160e23e8941be5b86419375b
sunxsoo <njs783075@gmail.com>
2023-09-26 00:46:53 +0900

    fix: 코스 추천 API 수정 retrofit 제거 (#41)

--- lib/course/repository/course_repository.dart
+++ lib/course/repository/course_repository.dart
@@ -3,27 +3,40 @@ import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart' hide Headers;
 import 'package:tago_app/common/const/data.dart';
 import 'package:tago_app/common/dio/dio.dart';
-import 'package:tago_app/common/model/cursor_pagination_model.dart';
-import 'package:tago_app/course/model/course_params.dart';
-import 'package:tago_app/trip/model/trip_model.dart';
-
-part 'course_repository.g.dart';
+import 'package:tago_app/course/model/course_reponse_model.dart';
 
 final courseRepositoryProvider = Provider<CourseRepository>((ref) {
   final dio = ref.watch(dioProvider);
-  final repository = CourseRepository(dio, baseUrl: '$ip/api/v1/courses');
+  final repository = CourseRepository(dio: dio, baseUrl: '$ip/api/v1/courses');
   return repository;
 });
 
-@RestApi()
-abstract class CourseRepository {
-  factory CourseRepository(Dio dio, {String baseUrl}) = _CourseRepository;
+class CourseRepository {
+  final String baseUrl;
+  final Dio dio;
 
-  @GET('/recommend')
-  @Headers({
-    'accessToken': 'true',
-  })
-  Future<CursorPagination<TripModel>> paginate({
-    @Queries() required CourseParams params,
+  CourseRepository({
+    required this.baseUrl,
+    required this.dio,
   });
+
+  Future<CourseResponseModel> recommendCourse({
+    required int? placeId,
+    required List<String> tags,
+  }) async {
+    final resp = await dio.request(
+      '$baseUrl/recommend',
+      queryParameters: {'placeId': placeId, 'tags': tags},
+      options: Options(
+        method: HttpMethod.GET,
+        headers: {
+          'accessToken': 'true',
+        },
+      ),
+    );
+
+    return CourseResponseModel.fromJson(
+      resp.data,
+    );
+  }
 }
