cc92f23748272a219ebbdd0abdd22cc12822b0dc
sunxsoo <njs783075@gmail.com>
2023-09-29 16:35:20 +0900

    setting: MaterialThemeData 버튼 ripple 효과 통일 (#42)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:tago_app/common/const/colors.dart';
 import 'package:tago_app/common/rotuer/go_router.dart';
 
 import 'package:intl/date_symbol_data_local.dart';
@@ -30,6 +31,8 @@ class _App extends ConsumerWidget {
     return MaterialApp.router(
       theme: ThemeData(
         fontFamily: 'Inter',
+        splashColor: LABEL_BG_COLOR,
+        highlightColor: Colors.transparent,
       ),
       debugShowCheckedModeBanner: false,
       routerConfig: router,
