335e4d94365c28b88c513ab3e6d0b10ec9e8ed08
sunxsoo <njs783075@gmail.com>
2023-10-03 22:21:20 +0900

    hotfix: TripList 여행 필터 스크롤 제외 목적 sliver 분기

--- lib/trip/view/list/trip_list_screen.dart
+++ lib/trip/view/list/trip_list_screen.dart
@@ -68,68 +68,6 @@ class _TripListScreenState extends ConsumerState<TripListScreen> {
     }
   }
 
-  Widget _buildListContent(CursorPagination cp, bool showRecommendation) {
-    return ListView.builder(
-      controller: controller,
-      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      itemBuilder: (context, index) {
-        if (showRecommendation && index == 0) {
-          final tripRecommendData = ref.watch(recommendProvider);
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                '당신에게 딱 맞는 여행!',
-                style: TextStyle(
-                  fontSize: 19.0,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              tripRecommendData == null
-                  ? const TripRecommendShimmerCard()
-                  : (tripRecommendData is TripModel)
-                      ? TripRecommendCard.fromModel(model: tripRecommendData)
-                      : Container(),
-              const SizedBox(height: 20.0),
-            ],
-          );
-        }
-
-        int tripIndex = showRecommendation ? index - 1 : index;
-        DateTime? previousDate;
-        if (tripIndex > 0) {
-          previousDate = cp.contents[tripIndex - 1].dateTime;
-        }
-
-        final bool shouldShowDate = previousDate == null ||
-            !DataUtils.isSameDate(
-                previousDate, cp.contents[tripIndex].dateTime);
-
-        if (shouldShowDate) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                DataUtils.formatDate(cp.contents[tripIndex].dateTime),
-                style: const TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              TripCard.fromModel(model: cp.contents[tripIndex]),
-            ],
-          );
-        } else {
-          return TripCard.fromModel(model: cp.contents[tripIndex]);
-        }
-      },
-      itemCount:
-          showRecommendation ? cp.contents.length + 1 : cp.contents.length,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final contents = ref.watch(tripProvider);
@@ -144,10 +82,7 @@ class _TripListScreenState extends ConsumerState<TripListScreen> {
           height: 55,
           child: FloatingActionButton.extended(
             shape: const CircleBorder(),
-            label: const Icon(
-              Icons.add,
-              size: 45,
-            ),
+            label: const Icon(Icons.add, size: 45),
             onPressed: () {
               context.go('/tripForm1');
             },
@@ -164,81 +99,183 @@ class _TripListScreenState extends ConsumerState<TripListScreen> {
                 sameGender: isSameGenderSelected,
               );
         },
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 30.0, top: 15.0),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: _toggleGenderFilter,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 90,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: isSameGenderSelected
-                            ? PRIMARY_COLOR
-                            : LABEL_BG_COLOR,
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                      ),
-                      child: Text(
-                        isSameGenderSelected ? "동성만" : '성별',
-                        style: TextStyle(
-                          fontSize: 13.0,
-                          color: isSameGenderSelected
-                              ? Colors.white
-                              : Colors.black,
-                          fontWeight: isSameGenderSelected
-                              ? FontWeight.w700
-                              : FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 15.0),
-                  GestureDetector(
-                    onTap: _togglePetFilter,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 90,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: isPetSelected ? PRIMARY_COLOR : LABEL_BG_COLOR,
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                      ),
-                      child: Text(
-                        isPetSelected ? "좋아요!" : '반려동물',
-                        style: TextStyle(
-                          fontSize: 13.0,
-                          color: isPetSelected ? Colors.white : Colors.black,
-                          fontWeight:
-                              isPetSelected ? FontWeight.w700 : FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+        child: CustomScrollView(
+          controller: controller,
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: false,
+              floating: true,
+              snap: false,
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+              flexibleSpace: FilterBar(
+                isSameGenderSelected: isSameGenderSelected,
+                isPetSelected: isPetSelected,
+                onToggleGenderFilter: _toggleGenderFilter,
+                onTogglePetFilter: _togglePetFilter,
               ),
+              expandedHeight: 70.0,
             ),
-            Expanded(
-              child: contents is CursorPaginationLoading
-                  ? (isPetSelected || isSameGenderSelected)
-                      ? const TripListSkeleton(
-                          isRecommend: false,
-                        )
-                      : const TripListSkeleton(
-                          isRecommend: true,
-                        )
-                  : _buildListContent(
-                      contents as CursorPagination, showRecommendation),
-            ),
+            if (contents is CursorPaginationLoading &&
+                (isPetSelected || isSameGenderSelected))
+              const SliverFillRemaining(
+                  child: TripListSkeleton(
+                isRecommend: false,
+              )),
+            if (contents is CursorPaginationLoading &&
+                (!isPetSelected && !isSameGenderSelected))
+              const SliverFillRemaining(
+                  child: TripListSkeleton(
+                isRecommend: true,
+              )),
+            if (contents is! CursorPaginationLoading)
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      if (showRecommendation && index == 0) {
+                        final tripRecommendData = ref.watch(recommendProvider);
+                        return Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                '당신에게 딱 맞는 여행!',
+                                style: TextStyle(
+                                  fontSize: 19.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              const SizedBox(height: 10.0),
+                              tripRecommendData == null
+                                  ? const TripRecommendShimmerCard()
+                                  : (tripRecommendData is TripModel)
+                                      ? TripRecommendCard.fromModel(
+                                          model: tripRecommendData)
+                                      : Container(),
+                              const SizedBox(height: 20.0),
+                            ],
+                          ),
+                        );
+                      }
+
+                      int tripIndex = showRecommendation ? index - 1 : index;
+                      DateTime? previousDate;
+                      if (tripIndex > 0) {
+                        previousDate =
+                            (contents).contents[tripIndex - 1].dateTime;
+                      }
+
+                      final bool shouldShowDate = previousDate == null ||
+                          !DataUtils.isSameDate(previousDate,
+                              (contents).contents[tripIndex].dateTime);
+
+                      if (shouldShowDate) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              DataUtils.formatDate(
+                                  (contents).contents[tripIndex].dateTime),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(height: 20.0),
+                            TripCard.fromModel(
+                                model: (contents).contents[tripIndex]),
+                          ],
+                        );
+                      } else {
+                        return TripCard.fromModel(
+                            model: (contents).contents[tripIndex]);
+                      }
+                    },
+                    childCount: showRecommendation
+                        ? (contents as CursorPagination).contents.length + 1
+                        : (contents as CursorPagination).contents.length,
+                  ),
+                ),
+              ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class FilterBar extends StatelessWidget {
+  final bool isSameGenderSelected;
+  final bool isPetSelected;
+  final Function() onToggleGenderFilter;
+  final Function() onTogglePetFilter;
+
+  const FilterBar({
+    super.key,
+    required this.isSameGenderSelected,
+    required this.isPetSelected,
+    required this.onToggleGenderFilter,
+    required this.onTogglePetFilter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30.0, top: 15.0),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: onToggleGenderFilter,
+            child: Container(
+              alignment: Alignment.center,
+              width: 90,
+              height: 30,
+              decoration: BoxDecoration(
+                color: isSameGenderSelected ? PRIMARY_COLOR : LABEL_BG_COLOR,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Text(
+                isSameGenderSelected ? "동성만" : '성별',
+                style: TextStyle(
+                  fontSize: 13.0,
+                  color: isSameGenderSelected ? Colors.white : Colors.black,
+                  fontWeight:
+                      isSameGenderSelected ? FontWeight.w700 : FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 15.0),
+          GestureDetector(
+            onTap: onTogglePetFilter,
+            child: Container(
+              alignment: Alignment.center,
+              width: 90,
+              height: 30,
+              decoration: BoxDecoration(
+                color: isPetSelected ? PRIMARY_COLOR : LABEL_BG_COLOR,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Text(
+                isPetSelected ? "좋아요!" : '반려동물',
+                style: TextStyle(
+                  fontSize: 13.0,
+                  color: isPetSelected ? Colors.white : Colors.black,
+                  fontWeight: isPetSelected ? FontWeight.w700 : FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
