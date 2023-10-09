a9733ea85d320b716b40af7938f1559e39e99a6a
sunxsoo <njs783075@gmail.com>
2023-10-10 01:57:14 +0900

    feat: 여행 제안 provider 구현 (#55)

---
+++ lib/trip/provider/trip_origin_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tago_app/trip/model/trip_model.dart';
+import 'package:tago_app/trip/repository/trip_repository.dart';
+
+final originProvider =
+    StateNotifierProvider<OriginNotifier, TripBaseModel?>((ref) {
+  final repository = ref.watch(tripRepositoryProvider);
+
+  final notifier = OriginNotifier(repository: repository);
+
+  return notifier;
+});
+
+class OriginNotifier extends StateNotifier<TripBaseModel?> {
+  final TripRepository repository;
+
+  OriginNotifier({
+    required this.repository,
+  }) : super(null);
+
+  Future<void> fetchOriginTrips() async {
+    try {
+      final data = await repository.getTagoOriginTrips();
+
+      state = data;
+    } catch (error) {
+      state = TripErrorModel();
+      print(error);
+    }
+  }
+}
