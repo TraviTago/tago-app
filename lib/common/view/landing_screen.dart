d090e2dbda322bc9a3ff01be42bffb956b701061
sunxsoo <njs783075@gmail.com>
2023-10-09 15:36:37 +0900

    hotfix: 랜딩 페이지 스와이퍼 이미지 적용

--- lib/common/view/landing_screen.dart
+++ lib/common/view/landing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tago_app/common/const/colors.dart';
@@ -13,7 +14,7 @@ class LandingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        padding: const EdgeInsets.symmetric(vertical: 30.0),
         child: SafeArea(
           top: true,
           bottom: true,
@@ -25,85 +26,88 @@ class LandingScreen extends StatelessWidget {
                   child: _Logo(),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                          ),
-                          minimumSize: MaterialStateProperty.all<Size>(
-                              Size(MediaQuery.of(context).size.width, 45)),
-                          elevation: MaterialStateProperty.all(0),
-                          backgroundColor:
-                              MaterialStateProperty.all(PRIMARY_COLOR),
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.white),
-                        ),
-                        onPressed: () {
-                          context.push('/login');
-                        },
-                        child: const Text(
-                          '로그인',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16.0),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                          ),
-                          minimumSize: MaterialStateProperty.all<Size>(
-                              Size(MediaQuery.of(context).size.width, 45)),
-                          elevation: MaterialStateProperty.all(0),
-                          backgroundColor:
-                              MaterialStateProperty.all(LABEL_BG_COLOR),
-                        ),
-                        onPressed: () {
-                          context.push('/signup');
-                        },
-                        child: const Text(
-                          '회원가입',
-                          style: TextStyle(
-                              color: PRIMARY_COLOR,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16.0),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              context.push('/driverLogin');
-                            },
-                            child: const Text(
-                              '기사님으로 로그인하기',
-                              style: TextStyle(
-                                color: LABEL_TEXT_SUB_COLOR,
-                                fontSize: 11.0,
-                                fontWeight: FontWeight.w500,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0),
                               ),
                             ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                                Size(MediaQuery.of(context).size.width, 45)),
+                            elevation: MaterialStateProperty.all(0),
+                            backgroundColor:
+                                MaterialStateProperty.all(PRIMARY_COLOR),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.white),
                           ),
-                        ],
-                      ),
-                    ],
+                          onPressed: () {
+                            context.push('/login');
+                          },
+                          child: const Text(
+                            '시작하기',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16.0),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                            ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                                Size(MediaQuery.of(context).size.width, 45)),
+                            elevation: MaterialStateProperty.all(0),
+                            backgroundColor:
+                                MaterialStateProperty.all(LABEL_BG_COLOR),
+                          ),
+                          onPressed: () {
+                            context.push('/signup');
+                          },
+                          child: const Text(
+                            '회원가입',
+                            style: TextStyle(
+                                color: PRIMARY_COLOR,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16.0),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                context.push('/driverLogin');
+                              },
+                              child: const Text(
+                                '기사님으로 로그인하기',
+                                style: TextStyle(
+                                  color: LABEL_TEXT_SUB_COLOR,
+                                  fontSize: 11.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -120,9 +124,47 @@ class _Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'asset/img/logo_red.png',
-      width: MediaQuery.of(context).size.width / 2,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'asset/img/logo_red.png',
+          width: MediaQuery.of(context).size.width / 4,
+        ),
+        const LandingSwiper(),
+      ],
+    );
+  }
+}
+
+class LandingSwiper extends StatelessWidget {
+  const LandingSwiper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 1.1,
+      height: MediaQuery.of(context).size.width * 1.1,
+      child: Swiper(
+        autoplay: true,
+        loop: true,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.asset(
+            'asset/img/onBoarding/${index + 1}.png',
+            width: MediaQuery.of(context).size.width,
+          );
+        },
+        indicatorLayout: PageIndicatorLayout.SCALE,
+        pagination: const SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            size: 6,
+            activeColor: PRIMARY_COLOR,
+            color: SELECTED_BOX_BG_COLOR,
+          ),
+        ),
+        itemCount: 4,
+        scale: 1,
+      ),
     );
   }
 }
